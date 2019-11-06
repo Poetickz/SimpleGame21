a4a216adfb7006a9364aeae214d4870d80808ed5
Alan Rocha <alan.rocha@udem.edu>
2019-11-06 00:47:41 -0600

    Added Deck Class

---
+++ Deck.rb
@@ -0,0 +1,28 @@
+require_relative 'Card'
+
+class Deck
+
+    attr_accessor :deck_cards
+
+
+    def initialize
+        @deck_cards = []
+        setting_up_deck
+    end
+
+    def setting_up_deck
+        @deck_cards.concat(build_set("♦"))
+        @deck_cards.concat(build_set("♠"))
+        @deck_cards.concat(build_set("♥"))
+        @deck_cards.concat(build_set("♣"))
+        @deck_cards.shuffle!
+    end
+
+    def build_set(symbol)
+        cards = []
+        value_cards = ["A","2","3","4","5","6","7","8","9","J","Q","K"]
+        value_cards.each { |value| cards.push(Card.new(symbol,value)) }
+        return cards
+    end
+
+end
